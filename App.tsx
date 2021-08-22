dc79724fcb1756522a1550efddd261d9a44af9ee
aliciamendes <aliealie.am@gmail.com>
2021-08-21 22:54:50 -0300

    created routes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Confirmation } from "./src/screen/Confirmation";
+import AppLoading from "expo-app-loading";
+import Routes from "./src/routes/index";
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from "@expo-google-fonts/jost";
-import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,6 +16,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-
-  return <Confirmation />;
+  return <Routes />;
 }
